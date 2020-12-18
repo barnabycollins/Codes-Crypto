c0b90773b0d4f810ffd51342be7dd65dd9e6d1ba
Barnaby Collins <barnabyc@sky.com>
2020-12-18 17:09:50 +0000

    Add adjustment of instance length

--- codes/get_frequencies.py
+++ codes/get_frequencies.py
@@ -1,15 +1,19 @@
 from collections import Counter
 from utils import getFileFromCommand
+import re
 
 (fileInfo, fileName) = getFileFromCommand()
 
 contents = open(fileName, 'r').read()
 
-counts = Counter(contents)
+instanceLength = 8
+instances = re.findall(".{" + str(instanceLength) + "}", contents)
+
+counts = Counter(instances)
 
 counts_sorted = sorted(list(dict(counts).items()), key=lambda x: x[1])
 
-maxLength = counts_sorted[-1][1]
+maxCount = counts_sorted[-1][1]
 
 for i in counts_sorted:
-    print("{:4} {:6} {}".format(repr(i[0]), i[1], '#'*round(128*i[1]/maxLength)))
+    print("{:24} {:6} {}".format(repr(i[0]), i[1], '#'*round(128*i[1]/maxCount)))
