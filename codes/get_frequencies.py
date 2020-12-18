96591f04b05326c86071809f1948e4cdc61f5418
Barnaby Collins <barnabyc@sky.com>
2020-12-18 17:13:22 +0000

    Truncate frequency list to get results faster

--- codes/get_frequencies.py
+++ codes/get_frequencies.py
@@ -15,5 +15,5 @@ counts_sorted = sorted(list(dict(counts).items()), key=lambda x: x[1])
 
 maxCount = counts_sorted[-1][1]
 
-for i in counts_sorted:
+for i in counts_sorted[-1000:]:
     print("{:24} {:6} {}".format(repr(i[0]), i[1], '#'*round(128*i[1]/maxCount)))
